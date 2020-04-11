2c5067b0669973efc307a330f26a397ae06bcdd6
Frank Liu <frankfliu2000@gmail.com>
2020-04-10 22:10:35 -0700

    Fix Covid19Detection criteria (#9)

--- covid19-detection/src/main/java/com/examples/Covid19Detection.java
+++ covid19-detection/src/main/java/com/examples/Covid19Detection.java
@@ -10,11 +10,9 @@
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-
 package com.examples;
 
 import ai.djl.MalformedModelException;
-import ai.djl.Model;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.util.BufferedImageUtils;
@@ -24,11 +22,13 @@ import ai.djl.ndarray.NDList;
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.repository.zoo.ModelZoo;
+import ai.djl.repository.zoo.ZooModel;
 import ai.djl.translate.TranslateException;
 import ai.djl.translate.Translator;
 import ai.djl.translate.TranslatorContext;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -36,20 +36,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Covid19Detection {
+
     private static final Logger logger = LoggerFactory.getLogger(Covid19Detection.class);
 
     public static void main(String[] args)
             throws IOException, MalformedModelException, TranslateException,
                     ModelNotFoundException {
-        String imagePath = args[0];
-        Criteria.Builder<BufferedImage, Classifications> builder =
-                Criteria.builder().setTypes(BufferedImage.class, Classifications.class);
+        String imagePath;
+        if (args.length == 0) {
+            imagePath =
+                    "https://github.com/ieee8023/covid-chestxray-dataset/blob/master/images/01E392EE-69F9-4E33-BFCE-E5C968654078.jpeg?raw=true";
+            logger.info("Input image not specified, using image:\n\t{}", imagePath);
+        } else {
+            imagePath = args[0];
+        }
+        BufferedImage image;
+        if (imagePath.startsWith("http")) {
+            image = BufferedImageUtils.fromUrl(new URL(imagePath));
+        } else {
+            image = BufferedImageUtils.fromFile(Paths.get(imagePath));
+        }
 
-        try (Model model = ModelZoo.loadModel(builder.build())) {
-            try (Predictor<BufferedImage, Classifications> predictor =
-                    model.newPredictor(new MyTranslator())) {
-                Classifications result =
-                        predictor.predict(BufferedImageUtils.fromFile(Paths.get(imagePath)));
+        Criteria<BufferedImage, Classifications> criteria =
+                Criteria.builder()
+                        .setTypes(BufferedImage.class, Classifications.class)
+                        .optTranslator(new MyTranslator())
+                        .build();
+
+        try (ZooModel<BufferedImage, Classifications> model = ModelZoo.loadModel(criteria)) {
+            try (Predictor<BufferedImage, Classifications> predictor = model.newPredictor()) {
+                Classifications result = predictor.predict(image);
                 logger.info("Diagnose:");
                 logger.info(result.toString());
             }
@@ -58,11 +74,7 @@ public class Covid19Detection {
 
     private static final class MyTranslator implements Translator<BufferedImage, Classifications> {
 
-        private List<String> classes;
-
-        public MyTranslator() {
-            classes = Arrays.asList("covid-19", "normal");
-        }
+        private static final List<String> CLASSES = Arrays.asList("covid-19", "normal");
 
         @Override
         public NDList processInput(TranslatorContext ctx, BufferedImage input) {
@@ -76,7 +88,7 @@ public class Covid19Detection {
         @Override
         public Classifications processOutput(TranslatorContext ctx, NDList list) {
             NDArray probabilities = list.singletonOrThrow();
-            return new Classifications(classes, probabilities);
+            return new Classifications(CLASSES, probabilities);
         }
     }
 }
