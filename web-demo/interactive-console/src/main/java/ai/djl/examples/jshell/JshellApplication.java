58c4e4e338f12c707ca1c2df13311f2cb3d0978d
Lanking <lanking520@live.com>
2020-05-22 12:04:12 -0700

    add backend logic for shell (#24)

---
+++ web-demo/interactive-console/src/main/java/ai/djl/examples/jshell/JshellApplication.java
@@ -0,0 +1,12 @@
+package ai.djl.examples.jshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JshellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JshellApplication.class, args);
+    }
+}
