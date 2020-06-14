0a6a7ba8c8d55ac5b0a3fc26a94b637f2127b11e
Frank Liu <frankfliu2000@gmail.com>
2020-06-14 09:55:35 -0700

    Refactor beanstalk demo

---
+++ beanstalk-model-serving/src/main/java/com/example/doodle/DoodleController.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.example.doodle;
+
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.transform.ToTensor;
+import ai.djl.modality.cv.translator.ImageClassificationTranslator;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelZoo;
+import ai.djl.repository.zoo.ZooModel;
+import ai.djl.translate.Pipeline;
+import ai.djl.translate.TranslateException;
+import ai.djl.translate.Translator;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DoodleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DoodleController.class);
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final String MODEL_URL =
+            "https://alpha-djl-demos.s3.amazonaws.com/model/quickdraw/doodle_mobilenet.zip";
+
+    @Autowired ZooModel<Image, Classifications> model;
+
+    /**
+     * This method is the Rest endpoint where the user can post their images to run inference
+     * against a model of their choice using DJL!
+     *
+     * @param input the request body containing the image
+     * @return returns the top 5 probable items from the model output
+     * @throws IOException if failed read HTTP request
+     */
+    @PostMapping(value = "/doodle", consumes = "image/*")
+    public String handleRequest(InputStream input) throws IOException {
+        Image img = ImageFactory.getInstance().fromInputStream(input);
+        try (Predictor<Image, Classifications> predictor = model.newPredictor()) {
+            Classifications classifications = predictor.predict(img);
+            return GSON.toJson(classifications.topK(5));
+        } catch (RuntimeException | TranslateException e) {
+            logger.error("", e);
+            Map<String, String> error = new ConcurrentHashMap<>();
+            error.put("status", "Invoke failed: " + e.toString());
+            return GSON.toJson(error);
+        }
+    }
+
+    @Bean
+    public ZooModel<Image, Classifications> model() throws ModelException, IOException {
+        Translator<Image, Classifications> translator =
+                ImageClassificationTranslator.builder()
+                        .optFlag(Image.Flag.GRAYSCALE)
+                        .setPipeline(new Pipeline(new ToTensor()))
+                        .optApplySoftmax(true)
+                        .build();
+        Criteria<Image, Classifications> criteria =
+                Criteria.builder()
+                        .setTypes(Image.class, Classifications.class)
+                        .optModelUrls(MODEL_URL)
+                        .optTranslator(translator)
+                        .build();
+        return ModelZoo.loadModel(criteria);
+    }
+}
---
+++ beanstalk-model-serving/src/main/resources/static/app.js
@@ -0,0 +1,115 @@
+// add touch event: http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
+// set canvas id to variable
+var canvas = document.getElementById("draw");
+canvas.height = canvas.width * 0.75;
+
+// get canvas 2D context and set it to the correct size
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// add event listeners to specify when functions should be triggered
+canvas.addEventListener("mousemove", function (e) {
+  draw(e, true);
+});
+canvas.addEventListener("mousedown", setPosition);
+canvas.addEventListener("mouseenter", setPosition);
+canvas.addEventListener("mouseup", runInference);
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  var touch = e.touches[0];
+  setPosition(touch);
+});
+canvas.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  runInference();
+});
+canvas.addEventListener("touchmove", function (e) {
+  e.preventDefault();
+  var touch = e.touches[0];
+  draw(touch, false);
+});
+
+// last known position
+var pos = { x: 0, y: 0 };
+// new position from mouse events
+function setPosition(e) {
+  var rect = canvas.getBoundingClientRect();
+  pos.x = (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+  pos.y = (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+}
+
+// bounding box
+var box = {x1: 10000, y1: 10000, x2: 0, y2: 0}
+// update box
+function updateBox(x, y) {
+  box.x1 = Math.min(box.x1, x);
+  box.x2 = Math.max(box.x2, x);
+  box.y1 = Math.min(box.y1, y);
+  box.y2 = Math.max(box.y2, y);
+}
+
+function draw(e, isMouse) {
+  if (isMouse && e.buttons !== 1) return; // if mouse is pressed.....
+
+  ctx.beginPath(); // begin the drawing path
+
+  ctx.lineWidth = 5; // width of line
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = "white"; // hex color of line
+
+  ctx.moveTo(pos.x, pos.y); // from position
+  updateBox(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to position
+  updateBox(pos.x, pos.y);
+
+  ctx.stroke(); // draw it!
+}
+
+function getCroppedCanvas() {
+  var destCanvas = document.createElement("canvas");
+  var destCtx = destCanvas.getContext("2d");
+  var boxWidth = Math.max(box.y2 - box.y1, box.x2 - box.x1);
+  var boxHeight = boxWidth;
+  destCanvas.width = 64;
+  destCanvas.height = 64;
+  destCtx.drawImage(
+        canvas,
+        box.x1,box.y1,boxWidth,boxHeight,  // source rect with content to crop
+        0,0,64,64);
+  return destCanvas;
+}
+
+function clearMe() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+function runInference(e) {
+  const Url = "/doodle";
+  var destCanvas = getCroppedCanvas();
+  destCanvas.toBlob(function(blob) {
+      fetch(Url, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'image/png',
+        },
+        body: blob
+      })
+      .then(response => response.json())
+      .then(data => {
+        var responseString = "";
+        for (var idx = 0; idx < data.length; idx++) {
+          responseString += data[idx]["className"] + ",";
+        }
+        M.toast({html: responseString}, 2000);
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+      });
+  });
+}
