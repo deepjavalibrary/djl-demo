299b1f92a1304b69168132dd24dd9a6beb55a3f4
Frank Liu <frankfliu2000@gmail.com>
2020-06-23 17:10:08 -0700

    Build native-image only with specified profile

--- graalvm/src/main/java/com/example/ImageClassification.java
+++ graalvm/src/main/java/com/example/ImageClassification.java
@@ -17,48 +17,38 @@ import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.Image;
 import ai.djl.modality.cv.ImageFactory;
-import ai.djl.modality.cv.transform.CenterCrop;
-import ai.djl.modality.cv.transform.Resize;
-import ai.djl.modality.cv.transform.ToTensor;
-import ai.djl.modality.cv.translator.ImageClassificationTranslator;
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelZoo;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
-import ai.djl.translate.Translator;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class ImageClassification {
 
-    private static final String MODEL_URL = "https://djl-tensorflow-javacpp.s3.amazonaws.com/tensorflow-models/chest_x_ray/saved_model.zip";
-    private static final String IMAGE_URL = "https://djl-ai.s3.amazonaws.com/resources/images/chest_xray.jpg";
-    private static final List<String> CLASSES = Arrays.asList("Normal", "Pneumonia");
+    private static final String IMAGE_URL = "https://djl-ai.s3.amazonaws.com/resources/images/kitten_small.jpg";
 
     public static void main(String[] args) throws ModelException, IOException, TranslateException {
         // java default ImageIO doesn't work with GraalVM
         ImageFactory.setImageFactory(new GraalvmImageFactory());
 
-        Translator<Image, Classifications> translator = ImageClassificationTranslator.builder()
-                .addTransform(new CenterCrop())
-                .addTransform(new Resize(224, 224))
-                .addTransform(new ToTensor())
-                .optSynset(CLASSES)
-                .build();
+        Image image;
+        if (args.length == 0) {
+            image = ImageFactory.getInstance().fromUrl(IMAGE_URL);
+        } else {
+            image = ImageFactory.getInstance().fromUrl(args[0]);
+        }
+
         Criteria<Image, Classifications> criteria =
                 Criteria.builder()
                         .setTypes(Image.class, Classifications.class)
-                        .optModelUrls(MODEL_URL)
-                        .optTranslator(translator)
+                        .optArtifactId("resnet")
                         .optProgress(new ProgressBar())
                         .build();
 
         try (ZooModel<Image, Classifications> model = ModelZoo.loadModel(criteria);
              Predictor<Image, Classifications> predictor = model.newPredictor()) {
-            Image image = ImageFactory.getInstance().fromUrl(IMAGE_URL);
 
             Classifications result = predictor.predict(image);
             System.out.println(result.toString());
