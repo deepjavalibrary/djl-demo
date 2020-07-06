124636baedeeddd4d34d440ab6b3facedd6d7740
Frank Liu <lfengl@yahoo.com>
2020-07-05 22:11:11 -0700

    Add footwear classification demo project

---
+++ footwear_classification/src/main/java/com/examples/Inference.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.examples;
+
+import ai.djl.Model;
+import ai.djl.ModelException;
+import ai.djl.inference.Predictor;
+import ai.djl.modality.Classifications;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.transform.Resize;
+import ai.djl.modality.cv.transform.ToTensor;
+import ai.djl.modality.cv.translator.ImageClassificationTranslator;
+import ai.djl.translate.TranslateException;
+import ai.djl.translate.Translator;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/** Uses the model to generate a prediction called an inference */
+public class Inference {
+
+    public static void main(String[] args) throws ModelException, TranslateException, IOException {
+        // the location where the model is saved
+        Path modelDir = Paths.get("models");
+
+        // the path of image to classify
+        // 0-boots; 1-sandals; 2-shoes; 3-slippers
+        String imageFilePath = "images/Sandals/110101.32.jpg";
+
+        // Load the image file from the path
+        Image img = ImageFactory.getInstance().fromFile(Paths.get(imageFilePath));
+
+        try (Model model = Models.getModel()) { // empty model instance
+            // load the model
+            model.load(modelDir, Models.MODEL_NAME);
+
+            // define a translator for pre and post processing
+            // out of the box this translator converts images to ResNet friendly ResNet 18 shape
+            Translator<Image, Classifications> translator =
+                    ImageClassificationTranslator.builder()
+                            .addTransform(new Resize(Models.IMAGE_WIDTH, Models.IMAGE_HEIGHT))
+                            .addTransform(new ToTensor())
+                            .optApplySoftmax(true)
+                            .build();
+
+            // run the inference using a Predictor
+            try (Predictor<Image, Classifications> predictor = model.newPredictor(translator)) {
+                // holds the probability score per label
+                Classifications predictResult = predictor.predict(img);
+                System.out.println(predictResult);
+            }
+        }
+    }
+}
---
+++ footwear_classification/src/main/java/com/examples/Training.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ * with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package com.examples;
+
+import ai.djl.Model;
+import ai.djl.basicdataset.ImageFolder;
+import ai.djl.metric.Metrics;
+import ai.djl.modality.cv.transform.Resize;
+import ai.djl.modality.cv.transform.ToTensor;
+import ai.djl.ndarray.types.Shape;
+import ai.djl.repository.Repository;
+import ai.djl.training.DefaultTrainingConfig;
+import ai.djl.training.EasyTrain;
+import ai.djl.training.Trainer;
+import ai.djl.training.TrainingConfig;
+import ai.djl.training.TrainingResult;
+import ai.djl.training.dataset.RandomAccessDataset;
+import ai.djl.training.evaluator.Accuracy;
+import ai.djl.training.listener.TrainingListener;
+import ai.djl.training.loss.Loss;
+import ai.djl.translate.Pipeline;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * In training, multiple passes (or epochs) are made over the training data trying to find patterns
+ * and trends in the data, which are then stored in the model. During the process, the model is
+ * evaluated for accuracy using the validation data. The model is updated with findings over each
+ * epoch, which improves the accuracy of the model.
+ */
+public final class Training {
+
+    // represents number of training samples processed before the model is updated
+    private static final int BATCH_SIZE = 32;
+
+    // the number of passes over the complete dataset
+    private static final int EPOCHS = 10;
+
+    public static void main(String[] args) throws IOException {
+        // the location to save the model
+        Path modelDir = Paths.get("models");
+
+        // create ImageFolder dataset from directory
+        ImageFolder dataset = initDataset("images");
+        // Split the dataset set into training dataset and validate dataset
+        RandomAccessDataset[] datasets = dataset.randomSplit(8, 2);
+
+        // set loss function, which seeks to minimize errors
+        // loss function evaluates model's predictions against the correct answer (during training)
+        // higher numbers are bad - means model performed poorly; indicates more errors; want to
+        // minimize errors (loss)
+        Loss loss = Loss.softmaxCrossEntropyLoss();
+
+        // setting training parameters (ie hyperparameters)
+        TrainingConfig config = setupTrainingConfig(loss);
+
+        try (Model model = Models.getModel(); // empty model instance to hold patterns
+                Trainer trainer = model.newTrainer(config)) {
+            // metrics collect and report key performance indicators, like accuracy
+            trainer.setMetrics(new Metrics());
+
+            Shape inputShape = new Shape(1, 3, Models.IMAGE_HEIGHT, Models.IMAGE_HEIGHT);
+
+            // initialize trainer with proper input shape
+            trainer.initialize(inputShape);
+
+            // find the patterns in data
+            EasyTrain.fit(trainer, EPOCHS, datasets[0], datasets[1]);
+
+            // set model properties
+            TrainingResult result = trainer.getTrainingResult();
+            model.setProperty("Epoch", String.valueOf(EPOCHS));
+            model.setProperty(
+                    "Accuracy", String.format("%.5f", result.getValidateEvaluation("Accuracy")));
+            model.setProperty("Loss", String.format("%.5f", result.getValidateLoss()));
+
+            // save the model after done training for inference later
+            // model saved as shoeclassifier-0000.params
+            model.save(modelDir, Models.MODEL_NAME);
+
+            // save labels into model directory
+            Models.saveSynset(modelDir, dataset.getSynset());
+        }
+    }
+
+    private static ImageFolder initDataset(String datasetRoot) throws IOException {
+        ImageFolder dataset =
+                ImageFolder.builder()
+                        // retrieve the data
+                        .setRepository(Repository.newInstance("footwear", Paths.get(datasetRoot)))
+                        .optPipeline(
+                                // create preprocess pipeline
+                                new Pipeline()
+                                        .add(new Resize(Models.IMAGE_WIDTH, Models.IMAGE_HEIGHT))
+                                        .add(new ToTensor()))
+                        // random sampling; don't process the data in order
+                        .setSampling(BATCH_SIZE, true)
+                        .build();
+
+        dataset.prepare();
+        return dataset;
+    }
+
+    private static TrainingConfig setupTrainingConfig(Loss loss) {
+        return new DefaultTrainingConfig(loss)
+                .addEvaluator(new Accuracy())
+                .addTrainingListeners(TrainingListener.Defaults.logging());
+    }
+}
