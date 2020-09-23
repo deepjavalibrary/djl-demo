14d67dc8ddfc6159c49a0d788c4cfacc5520eeb0
Frank Liu <frankfliu2000@gmail.com>
2020-09-22 20:31:52 -0700

    Workaround tensorflow model zoo bug

--- quarkus/src/main/java/com/example/ImageClassification.java
+++ quarkus/src/main/java/com/example/ImageClassification.java
@@ -13,15 +13,20 @@
 package com.example;
 
 import ai.djl.ModelException;
+import ai.djl.engine.Engine;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.Classifications;
 import ai.djl.modality.cv.Image;
 import ai.djl.modality.cv.ImageFactory;
+import ai.djl.modality.cv.transform.Normalize;
+import ai.djl.modality.cv.transform.Resize;
+import ai.djl.modality.cv.translator.ImageClassificationTranslator;
 import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelZoo;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.translate.TranslateException;
+import ai.djl.translate.Translator;
 import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.imaging.Imaging;
 
@@ -38,6 +43,9 @@ public class ImageClassification {
 
     private static final String IMAGE_URL = "https://djl-ai.s3.amazonaws.com/resources/images/kitten_small.jpg";
 
+    private static final float[] MEAN = {103.939f, 116.779f, 123.68f};
+    private static final float[] STD = {1f, 1f, 1f};
+
     @Path("/detect")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -48,12 +56,27 @@ public class ImageClassification {
             image = ImageFactory.getInstance().fromImage(Imaging.getBufferedImage(is));
         }
 
-        Criteria<Image, Classifications> criteria =
-                Criteria.builder()
-                        .setTypes(Image.class, Classifications.class)
-                        .optArtifactId("resnet")
-                        .optProgress(new ProgressBar())
-                        .build();
+        Criteria<Image, Classifications> criteria;
+        if ("TensorFlow".equals(Engine.getInstance().getEngineName())) {
+            Translator<Image, Classifications> translator = ImageClassificationTranslator.builder()
+                    .addTransform(new Resize(224))
+                    .addTransform(new Normalize(MEAN, STD))
+                    .build();
+            criteria =
+                    Criteria.builder()
+                            .setTypes(Image.class, Classifications.class)
+                            .optArtifactId("resnet")
+                            .optTranslator(translator)
+                            .optProgress(new ProgressBar())
+                            .build();
+        } else {
+            criteria =
+                    Criteria.builder()
+                            .setTypes(Image.class, Classifications.class)
+                            .optArtifactId("resnet")
+                            .optProgress(new ProgressBar())
+                            .build();
+        }
 
         try (ZooModel<Image, Classifications> model = ModelZoo.loadModel(criteria);
              Predictor<Image, Classifications> predictor = model.newPredictor()) {
