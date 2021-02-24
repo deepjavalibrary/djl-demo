df08633026e9e0a665356e3c10b3ee40b99058c6
Tim Mattison <tim@mattison.org>
2021-02-24 10:31:18 -0500

    Refactor to reduce nesting

--- visualization/djl-ui-demo/src/main/java/org/example/MnistTraining.java
+++ visualization/djl-ui-demo/src/main/java/org/example/MnistTraining.java
@@ -17,6 +17,7 @@ import ai.djl.training.listener.TrainingListener;
 import ai.djl.training.loss.Loss;
 import ai.djl.training.util.ProgressBar;
 import ai.djl.ui.listener.UiTrainingListener;
+import io.vavr.control.Try;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -30,31 +31,52 @@ public final class MnistTraining {
         // Construct neural network
         Block block = new Mlp(Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH, Mnist.NUM_CLASSES, new int[]{128, 64});
 
-        try (Model model = Model.newInstance(MODEL_NAME)) {
-            model.setBlock(block);
+        Model model = Try.of(() -> Model.newInstance(MODEL_NAME))
+                .map(newModel -> setBlock(newModel, block))
+                // Rethrow exceptions
+                .get();
 
-            // get training and validation dataset
-            RandomAccessDataset trainingSet = prepareDataset(Dataset.Usage.TRAIN, 64, Long.MAX_VALUE);
-            RandomAccessDataset validateSet = prepareDataset(Dataset.Usage.TEST, 64, Long.MAX_VALUE);
+        // get training and validation dataset
+        RandomAccessDataset trainingSet = prepareDataset(Dataset.Usage.TRAIN, 64, Long.MAX_VALUE);
+        RandomAccessDataset validateSet = prepareDataset(Dataset.Usage.TEST, 64, Long.MAX_VALUE);
 
-            // setup training configuration
-            DefaultTrainingConfig config = new DefaultTrainingConfig(Loss.softmaxCrossEntropyLoss())
-                    .addEvaluator(new Accuracy())
-                    .addTrainingListeners(TrainingListener.Defaults.logging())
-                    .addTrainingListeners(new UiTrainingListener())
-                    .addTrainingListeners(new CheckpointsTrainingListener(MODEL_DIR));
+        // setup training configuration
+        DefaultTrainingConfig config = new DefaultTrainingConfig(Loss.softmaxCrossEntropyLoss())
+                .addEvaluator(new Accuracy())
+                .addTrainingListeners(TrainingListener.Defaults.logging())
+                .addTrainingListeners(new UiTrainingListener())
+                .addTrainingListeners(new CheckpointsTrainingListener(MODEL_DIR));
 
-            try (Trainer trainer = model.newTrainer(config)) {
-                trainer.setMetrics(new Metrics());
+        Shape inputShape = new Shape(1, Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH);
 
-                Shape inputShape = new Shape(1, Mnist.IMAGE_HEIGHT * Mnist.IMAGE_WIDTH);
+        Metrics metrics = new Metrics();
 
-                // initialize trainer with proper input shape
-                trainer.initialize(inputShape);
-                EasyTrain.fit(trainer, 10, trainingSet, validateSet);
-            }
-            model.save(Paths.get(MODEL_DIR), MODEL_NAME);
-        }
+        Trainer trainer = Try.of(() -> model.newTrainer(config))
+                .map(newTrainer -> setMetrics(newTrainer, metrics))
+                .map(newTrainer -> initialize(newTrainer, inputShape))
+                // Rethrow exceptions
+                .get();
+
+        Try.run(() -> EasyTrain.fit(trainer, 10, trainingSet, validateSet))
+                // Rethrow exceptions
+                .get();
+
+        model.save(Paths.get(MODEL_DIR), MODEL_NAME);
+    }
+
+    private static Model setBlock(Model model, Block block) {
+        model.setBlock(block);
+        return model;
+    }
+
+    private static Trainer setMetrics(Trainer trainer, Metrics metrics) {
+        trainer.setMetrics(new Metrics());
+        return trainer;
+    }
+
+    private static Trainer initialize(Trainer trainer, Shape shape) {
+        trainer.initialize(shape);
+        return trainer;
     }
 
     private static RandomAccessDataset prepareDataset(Dataset.Usage usage, int batchSize, long limit) throws IOException {
