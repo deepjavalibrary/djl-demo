0e4fd005311968167a3aee3e61696093f7921b09
Xin Yang <xyang19@gmail.com>
2022-09-09 13:22:35 -0700

    Refactor Speech Recognition app

--- android/pytorch_android/speech_recognition/src/main/java/ai/djl/examples/speechrecognition/SpeechRecognitionActivity.java
+++ android/pytorch_android/speech_recognition/src/main/java/ai/djl/examples/speechrecognition/SpeechRecognitionActivity.java
@@ -14,14 +14,14 @@
 package ai.djl.examples.speechrecognition;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,67 +35,56 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import ai.djl.ModelException;
+import ai.djl.examples.speechrecognition.databinding.ActivityMainBinding;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.audio.Audio;
 import ai.djl.modality.audio.AudioFactory;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.translate.TranslateException;
 
-public class SpeechRecognitionActivity extends AppCompatActivity implements Runnable {
+public class SpeechRecognitionActivity extends AppCompatActivity {
 
-    private static final String TAG = SpeechRecognitionActivity.class.getName();
+    private static final String TAG = SpeechRecognitionActivity.class.getSimpleName();
+
+    private static final int REQUEST_RECORD_AUDIO = 13;
+    private static final int AUDIO_LEN_IN_SECOND = 6;
+    private static final int SAMPLE_RATE = 16000;
+    private static final int RECORDING_LENGTH = SAMPLE_RATE * AUDIO_LEN_IN_SECOND;
 
     private TextView mTextView;
     private Button mButton;
 
-    ZooModel<Audio, String> model;
-    Predictor<Audio, String> predictor;
-    Executor executor = Executors.newSingleThreadExecutor();
-
-    private final static int REQUEST_RECORD_AUDIO = 13;
-    private final static int AUDIO_LEN_IN_SECOND = 6;
-    private final static int SAMPLE_RATE = 16000;
-    private final static int RECORDING_LENGTH = SAMPLE_RATE * AUDIO_LEN_IN_SECOND;
-
-    private final static String LOG_TAG = SpeechRecognitionActivity.class.getSimpleName();
-
-    private int mStart = 1;
-    private HandlerThread mTimerThread;
-    private Handler mTimerHandler;
-    private Runnable mRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mTimerHandler.postDelayed(mRunnable, 1000);
-
-            SpeechRecognitionActivity.this.runOnUiThread(() -> {
-                mButton.setText(String.format("Listening - %ds left", AUDIO_LEN_IN_SECOND - mStart));
-                mStart += 1;
-            });
-        }
-    };
+    private ZooModel<Audio, String> model;
+    private Predictor<Audio, String> predictor;
+    private Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        mButton = findViewById(R.id.btnRecognize);
-        mTextView = findViewById(R.id.tvResult);
+        mButton = binding.btnRecognize;
+        mTextView = binding.tvResult;
 
         mButton.setOnClickListener(v -> {
-            mButton.setText(String.format("Listening - %ds left", AUDIO_LEN_IN_SECOND));
-            mButton.setEnabled(false);
+            mTextView.setText("");
 
-            Thread thread = new Thread(SpeechRecognitionActivity.this);
-            thread.start();
+            // Check for audio input permission
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
 
-            mTimerThread = new HandlerThread("Timer");
-            mTimerThread.start();
-            mTimerHandler = new Handler(mTimerThread.getLooper());
-            mTimerHandler.postDelayed(mRunnable, 1000);
+            // Start inference
+            executor.execute(new InferenceTask());
 
+            // Start timer
+            timer.start();
         });
+
         requestMicrophonePermission();
+
+        // Load model
         mButton.setText(getString(R.string.loader));
         mButton.setEnabled(false);
         executor.execute(new LoadModelTask());
@@ -109,45 +98,7 @@ public class SpeechRecognitionActivity extends AppCompatActivity implements Runn
         if (model != null) {
             model.close();
         }
-        stopTimerThread();
         super.onDestroy();
-    }
-
-    protected void stopTimerThread() {
-        mTimerThread.quitSafely();
-        try {
-            mTimerThread.join();
-            mTimerThread = null;
-            mTimerHandler = null;
-            mStart = 1;
-        } catch (InterruptedException e) {
-            Log.e(TAG, "Error on stopping background thread", e);
-        }
-    }
-
-    private class LoadModelTask implements Runnable {
-
-        @Override
-        public void run() {
-            try {
-                model = SpeechRecognitionModel.loadModel();
-                predictor = model.newPredictor();
-                runOnUiThread(() -> {
-                    mButton.setText("Start");
-                    mButton.setEnabled(true);
-                });
-            } catch (IOException | ModelException e) {
-                Log.e("SpeechRecognition", null, e);
-                runOnUiThread(() -> {
-                    AlertDialog alertDialog =
-                            new AlertDialog.Builder(SpeechRecognitionActivity.this).create();
-                    alertDialog.setTitle("Error");
-                    alertDialog.setMessage("Failed to load model");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            (dialog, which) -> finish());
-                });
-            }
-        }
     }
 
     private void requestMicrophonePermission() {
@@ -157,66 +108,101 @@ public class SpeechRecognitionActivity extends AppCompatActivity implements Runn
         }
     }
 
-    public void run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
+    private final CountDownTimer timer = new CountDownTimer(AUDIO_LEN_IN_SECOND * 1000, 1000) {
 
-        int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-
-        // check for audio input permission
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        @Override
+        public void onTick(long millisUntilFinished) {
+            int secondsRemaining = Math.toIntExact(millisUntilFinished / 1000);
+            mButton.setText(getResources().getString(R.string.countdown, secondsRemaining));
+            mButton.setEnabled(false);
         }
 
-        AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE,
-                AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
-
-        if (record.getState() != AudioRecord.STATE_INITIALIZED) {
-            Log.e(LOG_TAG, "Audio Record can't initialize!");
-            return;
+        @Override
+        public void onFinish() {
+            mButton.setText(R.string.recognizing);
         }
-        record.startRecording();
+    };
 
-        long shortsRead = 0;
-        int recordingOffset = 0;
-        short[] audioBuffer = new short[bufferSize / 2];
-        short[] recordingBuffer = new short[RECORDING_LENGTH];
+    private class LoadModelTask implements Runnable {
 
-        // listen to live audio data and save it
-        while (shortsRead < RECORDING_LENGTH) {
-            int numberOfShort = record.read(audioBuffer, 0, audioBuffer.length);
-            shortsRead += numberOfShort;
-            System.arraycopy(audioBuffer, 0, recordingBuffer, recordingOffset, numberOfShort);
-            recordingOffset += numberOfShort;
+        @Override
+        public void run() {
+            try {
+                model = SpeechRecognitionModel.loadModel();
+                predictor = model.newPredictor();
+                runOnUiThread(() -> {
+                    mButton.setText(R.string.start);
+                    mButton.setEnabled(true);
+                });
+            } catch (IOException | ModelException e) {
+                Log.e(TAG, null, e);
+                runOnUiThread(() -> {
+                    AlertDialog alertDialog =
+                            new AlertDialog.Builder(SpeechRecognitionActivity.this).create();
+                    alertDialog.setTitle("Error");
+                    alertDialog.setMessage("Failed to load model");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(android.R.string.ok),
+                            (dialog, which) -> finish());
+                });
+            }
         }
+    }
 
-        record.stop();
-        record.release();
-        stopTimerThread();
+    private class InferenceTask implements Runnable {
 
-        runOnUiThread(() -> {
-            mButton.setText("Recognizing...");
-        });
+        @Override
+        @SuppressLint("MissingPermission")
+        public void run() {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO);
 
-        float[] floatInputBuffer = new float[RECORDING_LENGTH];
+            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
 
-        // feed in float values between -1.0f and 1.0f by dividing the signed 16-bit inputs.
-        for (int i = 0; i < RECORDING_LENGTH; i++) {
-            floatInputBuffer[i] = recordingBuffer[i] / (float) Short.MAX_VALUE;
+            AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+
+            if (record.getState() != AudioRecord.STATE_INITIALIZED) {
+                Log.e(TAG, "Audio Record can't initialize!");
+                return;
+            }
+            record.startRecording();
+
+            long shortsRead = 0;
+            int recordingOffset = 0;
+            short[] audioBuffer = new short[bufferSize / 2];
+            short[] recordingBuffer = new short[RECORDING_LENGTH];
+
+            // listen to live audio data and save it
+            while (shortsRead < RECORDING_LENGTH) {
+                int numberOfShort = record.read(audioBuffer, 0, audioBuffer.length);
+                shortsRead += numberOfShort;
+                System.arraycopy(audioBuffer, 0, recordingBuffer, recordingOffset, numberOfShort);
+                recordingOffset += numberOfShort;
+            }
+
+            record.stop();
+            record.release();
+
+            float[] floatInputBuffer = new float[RECORDING_LENGTH];
+
+            // feed in float values between -1.0f and 1.0f by dividing the signed 16-bit inputs.
+            for (int i = 0; i < RECORDING_LENGTH; i++) {
+                floatInputBuffer[i] = recordingBuffer[i] / (float) Short.MAX_VALUE;
+            }
+
+            String output = "";
+            try {
+                Audio audio = AudioFactory.getInstance().fromData(floatInputBuffer);
+                output = predictor.predict(audio);
+            } catch (TranslateException e) {
+                Log.e(TAG, null, e);
+            }
+
+            final String result = output;
+            runOnUiThread(() -> {
+                mTextView.setText(result);
+                mButton.setEnabled(true);
+                mButton.setText(R.string.start);
+            });
         }
-
-        String output = "";
-        try {
-            Audio audio = AudioFactory.getInstance().fromData(floatInputBuffer);
-            output = predictor.predict(audio);
-        } catch (TranslateException e) {
-            Log.e(LOG_TAG, null, e);
-        }
-
-        final String result = output;
-        runOnUiThread(() -> {
-            mTextView.setText(result);
-            mButton.setEnabled(true);
-            mButton.setText("Start");
-        });
     }
 }
