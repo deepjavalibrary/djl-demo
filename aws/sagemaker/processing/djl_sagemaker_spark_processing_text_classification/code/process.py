2fa7d96ae822cd59b1ee17503555939651bbfd39
Xin Yang <xyangx@amazon.com>
2023-04-18 11:53:30 -0700

    [spark] Add sagemaker processing notebook example

---
+++ aws/sagemaker/processing/djl_sagemaker_spark_processing_text_classification/code/process.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+from pyspark.sql import SparkSession
+from djl_spark.task.text import TextClassifier
+
+
+def main():
+    parser = argparse.ArgumentParser(description="app inputs and outputs")
+    parser.add_argument("--s3_input_bucket", type=str, help="s3 input bucket")
+    parser.add_argument("--s3_input_key_prefix", type=str, help="s3 input key prefix")
+    parser.add_argument("--s3_output_bucket", type=str, help="s3 output bucket")
+    parser.add_argument("--s3_output_key_prefix", type=str, help="s3 output key prefix")
+    args = parser.parse_args()
+
+    spark = SparkSession.builder.appName("sm-spark-djl-text-classification").getOrCreate()
+
+    df = spark.read.json("s3://" + os.path.join(args.s3_input_bucket, args.s3_input_key_prefix))
+
+    # Text lassification
+    classifier = TextClassifier(input_col="inputs",
+                            output_col="prediction",
+                            engine="PyTorch",
+                            model_url="djl://ai.djl.huggingface.pytorch/distilbert-base-uncased-finetuned-sst-2-english")
+    outputDf = classifier.classify(df)
+    outputDf.write.mode("overwrite").option("header","true").csv("s3://" + os.path.join(args.s3_output_bucket, args.s3_output_key_prefix))
+
+
+if __name__ == "__main__":
+    main()
