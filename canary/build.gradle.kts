e6c1849e1b840c7d8b03bddce6719fd0a548d908
Frank Liu <frankfliu2000@gmail.com>
2024-07-31 17:06:39 -0700

    [canary] Fixes canary build for PyTorch to 2.4.0

--- canary/build.gradle.kts
+++ canary/build.gradle.kts
@@ -14,7 +14,7 @@ val engine: String = getEnv("DJL_ENGINE", "pytorch-native-auto")
 val os = getOsName()
 val arch: String = if (System.getProperty("os.arch") == "amd64") "x86_64" else System.getProperty("os.arch")
 val stagingRepo = getEnv("DJL_STAGING", "")
-val ptVersion = getEnv("PT_VERSION", "2.4.0-SNAPSHOT")
+val ptVersion = getEnv("PT_VERSION", "2.4.0")
 val ptJniVersion = ptVersion.replace("-SNAPSHOT", "")
 
 repositories {
