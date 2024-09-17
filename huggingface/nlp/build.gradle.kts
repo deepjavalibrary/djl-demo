939b2c1d40b227c5fa696db9f11ab69ee037f76a
Frank Liu <frankfliu2000@gmail.com>
2024-09-17 09:55:44 -0700

    [ci] Updates gradle to 8.10.1 (#512)

--- huggingface/nlp/build.gradle.kts
+++ huggingface/nlp/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("UNCHECKED_CAST")
 
 plugins {
+    `java-library`
     application
 }
 apply(file("../../tools/gradle/javaFormatter.gradle.kts"))
@@ -32,4 +33,32 @@ tasks {
         systemProperties = System.getProperties().toMap() as Map<String, Any>
         systemProperties.remove("user.dir")
     }
+
+    sourceSets.main.get().java.files
+        .filter { it.readText().contains("public static void main(String[] args)") }
+        .map {
+            it.path.substringAfter("java${File.separatorChar}").replace(File.separatorChar, '.')
+                .substringBefore(".java")
+        }
+        .forEach { className ->
+            val taskName = className.substringAfterLast(".")
+
+            register<JavaExec>(name = taskName) {
+                classpath = sourceSets.main.get().runtimeClasspath
+                mainClass = className
+                group = "application"
+
+                allJvmArgs = allJvmArgs + listOf(
+                    "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                    "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED",
+                    "--add-opens=java.base/java.net=ALL-UNNAMED",
+                    "--add-opens=java.base/java.nio=ALL-UNNAMED",
+                    "--add-opens=java.base/java.time=ALL-UNNAMED",
+                    "--add-opens=java.base/java.util=ALL-UNNAMED",
+                    "--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED",
+                    "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED",
+                    "--add-opens=java.base/sun.util.calendar=ALL-UNNAMED"
+                )
+            }
+        }
 }
