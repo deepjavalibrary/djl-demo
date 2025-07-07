b74acb1fc8e76abd755eb896fc29b03b45b2ce4e
Xin Yang <xyangx@amazon.com>
2025-07-07 12:35:09 -0700

    Updates DJL version to 0.34.0-SNAPSHOT

--- djl-serving/workflows/multi-model/function/build.gradle.kts
+++ djl-serving/workflows/multi-model/function/build.gradle.kts
@@ -5,8 +5,8 @@ apply(file("../../../../tools/gradle/javaFormatter.gradle.kts"))
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
-var djlVersion = property("djl_version") as String
-djlVersion = if (djlVersion.endsWith("-SNAPSHOT")) djlVersion else "${djlVersion}-SNAPSHOT"
+// var djlVersion = property("djl_version") as String
+// djlVersion = if (djlVersion.endsWith("-SNAPSHOT")) djlVersion else "${djlVersion}-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("ai.djl:bom:${djlVersion}"))
+    implementation(platform("ai.djl:bom:0.28.0"))
     implementation("ai.djl:api")
     implementation("ai.djl.serving:serving")
 }
